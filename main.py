1e55e14d2b1896bed2ff5348c269bf87987e4ac7
PEI <paytonpei@cmail.carleton.ca>
2021-07-21 17:37:24 -0400

    remove slashs for irregular tickers

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ def courseAvailable(listOfItems):
         if i['color'] == 'red':
             flag = True
     return flag
-
+def removeSlash(items):
+    for i in range(len(items)):
+        if items[i][3].isalpha() == False:
+            items[i] = items[i][:3]
 def findCourseTicker(rawHTML):
     # Input:raw HTML, Type:String
     if rawHTML.find('href') == -1:
@@ -27,24 +30,22 @@ def findCourseTicker(rawHTML):
 
 if __name__ == "__main__":
     url = 'https://calendar.carleton.ca/undergrad/courses/'
-
-    html = urlopen(
-        url).read().decode()
+    html = urlopen(url).read().decode()
     soup = BeautifulSoup(html, features='lxml')
     abbr = soup.find_all("div", {"class": "course"})
+    # print(abbr)
     # for i in abbr:
     #     print(i)
     # item = abbr[7]
     # print(type(abbr))
     c = str(abbr)
-
+    print(type(c))
+    print(c)
     tickerList = []
     a = 'href="comp" href="nsms"'
     index= a.find('href')
     print(a.find('href'))
-    # tickerList.append(a[index+6:index+10])
-    #make new list
-    findCourseTicker(a)
+    findCourseTicker(c)
+    # print(tickerList)
+    removeSlash(tickerList)
     print(tickerList)
-    
-
