99fa0b3f33030f2150a80d406b5923603d879ed3
Payton Dong Pei <silence1198@outlook.com>
2021-09-14 16:15:17 -0400

    Update jour4401.py

--- jour4401.py
+++ jour4401.py
@@ -1,109 +1,68 @@
-import webbrowser
-import requests
-import sqlite3
-import sys
-import os
-import json
-import csv
-
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-
-def courseAvailable(listOfItems):
-    flag = False
-    for i in listOfItems:
-        if i['color'] == 'red':
-            flag = True
-    return flag
-def removeSlash(items):
-    for i in range(len(items)):
-        if items[i][3].isalpha() == False:
-            items[i] = items[i][:3]
-def findCourseTicker(rawHTML):
-    # Input:raw HTML, Type:String
-    if rawHTML.find('href') == -1:
-        #If there is no herf tag, means there are no more courses left.
-        print("End of the Recursion.")
-    else:
-
-        #rawHTML find will return the current the first index.
-        index = rawHTML.find('href')#return the 1st index
-        tickerList.append(rawHTML[index+6:index+10])
-        findCourseTicker(rawHTML[index+10:])
+import csv
+import time
 
 
-
-def write_to_csv(data):
-    with open('names.csv', 'w', newline='') as csvfile:
-        fieldnames = ['Bussiness Name', 'Operating Name']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        # for i in data:
-        # writer.writerow({'Bussiness Name': bussinessName, 'Operating Name': operatingName})
-
-def remove_chr(type,list):
-
-    for i in list:
-        if type == 1:
-                i = i.replace('\r',"").replace('\n',"").replace('\t',"")
-        elif type == 2:
-            if i == "\n":
-                i = i.replace('\n'," ")
-            else:
-                i = i.replace('\r',"").replace('\n',"").replace('\t',"")
+def create_csv():
+    path = "file.csv"
+    with open(path, 'w') as file:
+        csv_write = csv.writer(file)
+        csv_head = ['Business Name', 'Operating Name']
+        csv_write.writerow(csv_head)
 
 
-if __name__ == "__main__":
-    url = 'https://apps.cra-arc.gc.ca/ebci/hacc/cews/srch/pub/bscSrch?dsrdPg=1&q.srchNm=car&q.ordrClmn=NAME&q.ordrRnk=ASC'
+def write_csv(business: list[str], operating: list[str]):
+    path = "file.csv"
+    with open(path, 'a+') as f:
+        csv_write = csv.writer(f)
+        for i in range(100):
+            data_row = [business[i], operating[i]]
+            csv_write.writerow(data_row)
+
+
+def remove_chr(items):
+    lis = []
+    for item in items:
+        lis.append(item.replace('\n', "").replace('\r', "").replace('\t', ""))
+    return lis
+
+
+def craw_single_page(pagenum):
+    front = 'https://apps.cra-arc.gc.ca/ebci/hacc/cews/srch/pub/bscSrch?dsrdPg='
+    back = '&q.srchNm=car&q.ordrClmn=NAME&q.ordrRnk=ASC'
+    url = front + str(pagenum) + back
     html = urlopen(url).read().decode()
     soup = BeautifulSoup(html, features='lxml')
-    abbr = soup.find_all("div", {"class": "mrgn-tp-sm"})
-    
-    newAddr = soup.find_all("div",class_="mrgn-tp-sm")
-    bussinessName = []
+    newAddress = soup.find_all("div", class_="mrgn-tp-sm")
+    businessName = []
     operatingName = []
-
-    vv = 0
     flag = True
-
-    for i in newAddr:
-        # bussinessName.append(i.get_text().replace("\t","").replace("\r","").replace("\n",""))
-        # print(i.get_text().replace("\t","").replace("\r","").replace)
-        vv += 1
+    for i in newAddress:
         text = i.get_text()
         if flag:
-            bussinessName.append(text)
+            businessName.append(text)
             flag = not flag
         else:
             operatingName.append(text)
             flag = not flag
         # print(i.get_text())
+    newOperatingName = remove_chr(operatingName)
+    newBusinessName = remove_chr(businessName)
+    write_csv(newBusinessName, newOperatingName)
 
-    remove_chr(1,bussinessName)
-    remove_chr(2,operatingName)
-    print(bussinessName)
 
-    print("------------------------------")
-    print(operatingName)
+def page_range():
+    start, end = map(int, input("Please enter starting page and ending page").split())
+    for i in range(start, end + 1):
+        craw_single_page(i)
 
-    # print(len(operatingName))
-    # write_to_csv(newAddr)
-    
-    # c = str(abbr)
-    # print(type(c))
-    # print(c)
-    # tickerList = []
-    # a = 'href="comp" href="nsms"'
-    # index= a.find('href')
-    # print(a.find('href'))
-    # findCourseTicker(c)
-    # print(tickerList)
-    # removeSlash(tickerList)
-    # print(tickerList)
-    
-    # conn = sqlite3.connect('test.db')
-    # cursor = conn.cursor()
-    # for item in tickerList:
-    #     cursor.execute("Insert into course(name) Values(?)",[item])
-    # conn.commit()
-    # conn.close()
+
+if __name__ == "__main__":
+    start = time.time()
+    url = 'https://apps.cra-arc.gc.ca/ebci/hacc/cews/srch/pub/bscSrch?dsrdPg=1&q.srchNm=car&q.ordrClmn=NAME&q.ordrRnk=ASC'
+    create_csv()
+    page_range()
+    end = time.time()
+    print("end time:" + str(end - start))
+#end time:12.6405189037323
