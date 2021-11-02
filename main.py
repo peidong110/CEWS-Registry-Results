fb860d9245215082516a5e184e7984c89541fce3
Payton Dong Pei <silence1198@outlook.com>
2021-11-02 16:10:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,96 @@
-import webbrowser
-import requests
-import sqlite3
-import sys
-import os
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from twilio.rest import Client
+import csv
+import time
+import requests
 
-def courseAvailable(listOfItems):
-    flag = False
-    for i in listOfItems:
-        if i['color'] == 'red':
-            flag = True
-    return flag
-def removeSlash(items):
-    for i in range(len(items)):
-        if items[i][3].isalpha() == False:
-            items[i] = items[i][:3]
-def findCourseTicker(rawHTML):
-    # Input:raw HTML, Type:String
-    if rawHTML.find('href') == -1:
-        #If there is no herf tag, means there are no more courses left.
-        print("End of the Recursion.")
-    else:
 
-        #rawHTML find will return the current the first index.
-        index = rawHTML.find('href')#return the 1st index
-        tickerList.append(rawHTML[index+6:index+10])
-        findCourseTicker(rawHTML[index+10:])
 
-if __name__ == "__main__":
-    url = 'https://calendar.carleton.ca/undergrad/courses/'
+
+def create_csv():
+    path = "file.csv"
+    with open(path, 'w', newline='') as file:
+        csv_write = csv.writer(file)
+        csv_head = ['Business Name', 'Operating Name']
+        csv_write.writerow(csv_head)
+
+
+def write_csv(business: list, operating: list):
+    path = "file.csv"
+    with open(path, 'a+', newline='') as f:
+        csv_write = csv.writer(f)
+        for i in range(len(business)):
+            data_row = [business[i], operating[i]]
+            csv_write.writerow(data_row)
+
+
+def remove_chr(items):
+    lis = []
+    for item in items:
+        lis.append(item.replace('\n', "").replace('\r', "").replace('\t', ""))
+    return lis
+
+def getMax():
+    #This function is unnecessary if you know the max page though
+    max = 400
+    url = 'https://apps.cra-arc.gc.ca/ebci/hacc/cews/srch/pub/fllLstSrh?dsrdPg=400&q.ordrClmn=NAME&q.ordrRnk=ASC'
+    r= requests.get(url)
+    txt=r.text
+
+    # f = open('file.html', 'x', encoding='utf-8')
+    while(txt.find("We found no match for the search criteria you used.") < 0 ):
+      max+=1
+      url = 'https://apps.cra-arc.gc.ca/ebci/hacc/cews/srch/pub/fllLstSrh?dsrdPg='+str(max)+'&q.ordrClmn=NAME&q.ordrRnk=ASC'
+
+      r= requests.get(url)
+      txt=r.text
+      if(txt.find("We found no match for the search criteria you used.") > 0):
+        max = max-1
+        break;
+    return max
+
+
+
+def craw_single_page(pagenum):
+    # front = 'https://apps.cra-arc.gc.ca/ebci/hacc/cews/srch/pub/bscSrch?dsrdPg='
+    # back = '&q.srchNm=car&q.ordrClmn=NAME&q.ordrRnk=ASC'
+    front = 'https://apps.cra-arc.gc.ca/ebci/hacc/cews/srch/pub/fllLstSrh?dsrdPg='
+    back = '&q.ordrClmn=NAME&q.ordrRnk=ASC'
+    url = front + str(pagenum) + back
     html = urlopen(url).read().decode()
     soup = BeautifulSoup(html, features='lxml')
-    abbr = soup.find_all("div", {"class": "course"})
-    c = str(abbr)
-    print(type(c))
-    print(c)
-    tickerList = []
-    a = 'href="comp" href="nsms"'
-    # index= a.find('href')
-    # print(a.find('href'))
-    findCourseTicker(c)
-    # print(tickerList)
-    removeSlash(tickerList)
-    # print(tickerList)
-    
-    conn = sqlite3.connect('test.db')
-    cursor = conn.cursor()
-    for item in tickerList:
-        cursor.execute("Insert into course(name) Values(?)",[item])
-    conn.commit()
-    conn.close()
+    new_address = soup.find_all("div", class_="mrgn-tp-sm")
+    business_name = []
+    operating_name = []
+    flag = True
+    for i in new_address:
+        text = i.get_text()
+        if flag:
+            business_name.append(text)
+            flag = not flag
+        else:
+            operating_name.append(text)
+            flag = not flag
+        # print(i.get_text())
+    new_operating_name = remove_chr(operating_name)
+    new_business_name = remove_chr(business_name)
+    write_csv(new_operating_name, new_business_name)
+
+
+
+
+if __name__ == "__main__":
+    start = time.time()
+    url = 'https://apps.cra-arc.gc.ca/ebci/hacc/cews/srch/pub/bscSrch?dsrdPg=1&q.srchNm=car&q.ordrClmn=NAME&q.ordrRnk=ASC'
+    create_csv()
+    # page_range()
+    max = getMax()
+    print("Max Page:"+str(max))
+    for i in range(max):
+        print(i)
+        craw_single_page(i+1)
+
+    end = time.time()
+    print("end time:" + str(end - start))
+    #end time:12.6405189037323
+    # 1- 100 end time:55.16016626358032
